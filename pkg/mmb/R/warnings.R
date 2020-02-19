b92111a6c02a223748b49ce45e8cd677127f938d
Sebastian Hönel <sebastian.honel@lnu.se>
2020-02-19 14:55:09 +0100

    Add new methods setMessages() and getMessages()

--- pkg/mmb/R/warnings.R
+++ pkg/mmb/R/warnings.R
@@ -16,3 +16,24 @@ setWarnings <- function(enable = T) {
 #' @return Boolean to indicate whether warnings are enabled or not.
 #' @export
 getWarnings <- function() varWarn$get()
+
+
+varMsg <- make.varClosure(F)
+
+
+#' Setter for enabling or disabling messages. Messages are disabled by default.
+#' Use these to enable high verbosity.
+#' @author Sebastian Hönel <sebastian.honel@lnu.se>
+#' @param enable a boolean to indicate whether to enable messages or not.
+#' @return void
+#' @export
+setMessages <- function(enable = T) {
+  varMsg$set(!!enable)
+  varMsg$get()
+}
+
+#' Getter for the state of messages. Returns true if enabled.
+#' @author Sebastian Hönel <sebastian.honel@lnu.se>
+#' @return Boolean to indicate whether messages are enabled or not.
+#' @export
+getMessages <- function() varMsg$get()
